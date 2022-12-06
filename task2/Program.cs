8004c87714b047606102a396c997d85d885fd69b
Vladislav <vlad-taranin@yandex.ru>
2022-12-06 12:28:14 +0300

    сделал задание 2и4

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+Console.WriteLine("первое число больше второго");
+else
+Console.WriteLine("второе число больше первого");
---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N%2!=0)
+Console.WriteLine("нет");
+else
+Console.WriteLine("да");
